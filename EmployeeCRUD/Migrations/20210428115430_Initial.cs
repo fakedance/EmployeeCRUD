63e4754078b0fc064a3279d15ea182b6938435aa
fakedance <72011267+fakedance@users.noreply.github.com>
2021-04-28 17:25:00 +0530

    Add project files.

---
+++ EmployeeCRUD/Migrations/20210428115430_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeCRUD.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Designation = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Office = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmpId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EmployeeCRUD/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string LastName { get; set; }
+
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Designation { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Position { get; set; }
+        public string Office { get; set; }
+    }
+}
